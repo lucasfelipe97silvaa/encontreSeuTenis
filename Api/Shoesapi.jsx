9a9b3181219a7ca784c4c3d04e6b937414bbbd4f
LUCAS FELIPE MARCELINO DA SILVA <lucas.fmsilva10@senacsp.edu.br>
2023-08-10 22:10:18 -0300

    search-your-shoes

---
+++ Api/Shoesapi.jsx
@@ -0,0 +1,23 @@
+export function getShoes(){
+    return [
+        {
+            "title": "Tenis Nike Air Jordan 11",
+            "image": "https://imgnike-a.akamaihd.net/1920x1920/024813P1A2.jpg",
+            "price": 1129.99,
+            "url": "https://www.nike.com.br/air-jordan-11-024813.html?cor=P1"
+        },
+        {
+            "title": "Tenis Nike Dunk PineApple",
+            "image": "https://maze.cdn.plataformaneo.com.br/produto/multifotos/hd/20220627121302_1802998198_DZ.png",
+            "price": 699.99,
+            "url": "https://www.luiztools.com.br/livro-node-ii-amazon"
+        },
+        {
+            "title": "Tenis Nike Air Force 1",
+            "image": "https://maze.cdn.plataformaneo.com.br/produto/multifotos/hd/20230728153916_6341993659_DZ.png",
+            "price": 999.80,
+            "url": "https://www.luiztools.com.br/livro-mongodb-amazon"
+        }
+        ]
+}
+
